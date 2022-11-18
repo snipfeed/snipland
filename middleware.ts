a7b1eb1d7608b3ae063738d679c39257b7667099
Mouad Belbah <moadbel@gmail.com>
2022-11-18 14:37:44 +0100

    add default return to middleware

--- middleware.ts
+++ middleware.ts
@@ -5,4 +5,7 @@ export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/test-middleware')) {
     return NextResponse.rewrite(new URL('/mb9998', request.url))
   }
+
+  return NextResponse.next()
+
 }
