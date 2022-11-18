7225d77dbfa23144bd1f5919aa06b08b23369532
Mouad Belbah <moadbel@gmail.com>
2022-11-18 14:25:52 +0100

    test basic middleware

---
+++ pages/middleware.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/test-middleware')) {
+    return NextResponse.rewrite(new URL('/mb9998', request.url))
+  }
+}
